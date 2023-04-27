2448948b124dd728f27d846e998857d5a8b754c0
Ryan <frostcatlover@outlook.com>
2023-04-27 15:54:55 -0700

    2nd commit

--- server.js
+++ server.js
@@ -9,97 +9,97 @@ let poketypes = {
     'normal': {
         'effective': 'none',
         'ineffective': 'rock, steel',
-        'no effect': 'ghost',
+        'noEffect': 'ghost',
     },
     'fire': {
         'effective': 'grass, ice, bug, steel',
         'ineffective': 'fire, water, rock, dragon',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'water': {
         'effective': 'fire, ground, rock',
         'ineffective': 'water, grass, dragon',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'electric': {
         'effective': 'water, flying',
         'ineffective': 'electric, grass, dragon',
-        'no effect': 'ground',
+        'noEffect': 'ground',
     },
     'grass': {
         'effective': 'water, ground, rock',
         'ineffective': 'fire, grass, poison, flying, bug, dragon, steel',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'ice': {
         'effective': 'grass, ground, flying, dragon',
         'ineffective': 'fire, water, ice, steel',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'fighting': {
         'effective': 'normal, ice, rock, dark, steel',
         'ineffective': 'poinson, flying, psychic, bug, fairy',
-        'no effect': 'ghost',
+        'noEffect': 'ghost',
     },
     'poison': {
         'effective': 'grass, fairy',
         'ineffective': 'poison, ground, rock, ghost',
-        'no effect': 'steel',
+        'noEffect': 'steel',
     },
     'ground': {
         'effective': 'fire, electric, poison, rock, steel',
         'ineffective': 'grass, bug',
-        'no effect': 'flying',
+        'noEffect': 'flying',
     },
     'flying': {
         'effective': 'grass, fighting, bug',
         'ineffective': 'electric, rock, steel',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'psychic': {
         'effective': 'fighting, poison',
         'ineffective' : 'psychic, steel',
-        'no effect': 'dark',
+        'noEffect': 'dark',
     },
     'bug': {
         'effective': 'grass, psychic, dark',
         'ineffective': 'fire, fighting, poison, flying, ghost, steel, fairy',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'rock': {
         'effective': 'fire, ice, flying, bug',
         'ineffective': 'fighting, ground, steel',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'ghost': {
         'effective': 'psychic, ghost',
         'ineffective': 'dark',
-        'no effect': 'normal',
+        'noEffect': 'normal',
     },
     'dragon': {
         'effective': 'dragon',
         'ineffective': 'steel',
-        'no effect': 'fairy',
+        'noEffect': 'fairy',
     },
     'dark': {
         'effective': 'psychic, ghost',
         'ineffective': 'fighting, dark, fairy',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'steel': {
         'effective': 'ice, rock, fairy',
         'ineffective': 'fire, water, electric, steel',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'fairy': {
         'effective': 'fighting, dragon, dark',
         'ineffective': 'fire, poison, steel',
-        'no effect': 'none',
+        'noEffect': 'none',
     },
     'no input': {
         'effective': 'none',
         'ineffective': 'none',
-        'no effect': 'none',
+        'noEffect': 'none',
     }
 }
 
